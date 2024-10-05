391d62caee9d3c7534f1558cf032a08415cf932a
Armando Campos <camposa03@yahoo.com>
2024-10-05 13:38:39 -0600

    Adding api endpoints and documentation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Backend
+# Description
+
+This is a sample backend application written in node.js/express for handling HTTP requests
+
+
+## Testing
+
+To test this API, perform a `curl` command. For example
+
+```
+curl -d "name=tyler&age=21" -X POST http://localhost:3000/contact-us
+```
+
+To test a `GET` request:
+
+```
+curl localhost:3000
+```
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('node:fs');
+// To read our request (body)
+const bodyParser = require('body-parser');
 
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
+// Middleware to parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//localhost:3000
 app.get('/', (req, res) => {
   res.send('Hello World!');
+
 })
 
 app.listen(port, () => {
@@ -12,3 +22,35 @@ app.listen(port, () => {
 })
 
 
+app.post('/contact-us', (req, res) => {
+
+  console.log("What is the request", req.body);
+
+  // This is our response message
+  let response = {
+    name: req.body.name,
+    age: req.body.age
+  };
+
+  // save this information into a file (example)
+  let content = response.name + ":" + response.age;
+  writeToFile(content);
+  
+
+  // Send a json representation of our response
+  res.json(response);
+
+});
+
+
+function writeToFile(content) {
+
+  try {
+
+    fs.writeFileSync('./form.txt', content);
+    // file written successfully
+  } catch (err) {
+    console.error(err);
+  }
+
+}
